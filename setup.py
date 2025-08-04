8367f9c3c5ff8260dd3f1cd0e4c7563b680063fc
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-08-04 17:42:18 +0200

    test-pypi with only MacOS

--- setup.py
+++ setup.py
@@ -924,7 +924,7 @@ if use_gui():
 setup_requires = ["numpy"]
 
 distrib = setup(name="Pymca_release_test",
-                version='0.0.1.test',
+                version='0.0.1.dev0+test',
                 description=description,
                 author="V. Armando Sole",
                 license="MIT",
