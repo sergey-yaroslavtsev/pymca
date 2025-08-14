f21df5c1f9994b7ea52955ae6bedc9ae54907fa5
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-08-14 21:10:12 +0200

    test old existed tag

--- src/PyMca5/__init__.py
+++ src/PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = '1.2.2'
+__version__ = '1.0.1'
 
 import os
 import sys
