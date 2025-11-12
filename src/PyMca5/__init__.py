909b416b338d45a08901e9fc8a79852a2a72b1fb
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-11-12 13:35:07 +0100

    trying to add tests into the GUI to run through CI - trying on full release

--- src/PyMca5/__init__.py
+++ src/PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.9.5.dev43"
+__version__ = "5.9.5.dev44"
 
 import os
 import sys
