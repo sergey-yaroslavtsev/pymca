c4a2f01e561d1c718fe7aa441016113e53d3611a
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-11-14 19:47:27 +0100

    correct upload - because it is a fork

--- src/PyMca5/__init__.py
+++ src/PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.9.5.dev64"
+__version__ = "5.9.5.dev65"
 
 import os
 import sys
